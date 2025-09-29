acee3710b9d229db4f155476b64e970bbea8912f
Christian Maranan <leoslittledino1014@gmail.com>
2025-09-29 16:15:39 +0800

    Update HappyTeachersDay.js

--- teachers-day-app/src/HappyTeachersDay.js
+++ teachers-day-app/src/HappyTeachersDay.js
@@ -66,13 +66,13 @@ const HappyTeachersDay = () => {
         <img
           src={profIconLeft}
           alt="Professor Left"
-          className="w-20 h-20 rounded-full border-4 border-blue-400 shadow-lg object-cover"
+          className="w-16 h-16 rounded-full border-2 border-blue-400 shadow-lg object-cover"
         />
         <div className="flex-1" /> {/* Spacer */}
         <img
           src={profIconRight}
           alt="Professor Right"
-          className="w-20 h-20 rounded-full border-4 border-blue-400 shadow-lg object-cover"
+          className="w-16 h-16 rounded-full border-2 border-blue-400 shadow-lg object-cover"
         />
       </div>
 
